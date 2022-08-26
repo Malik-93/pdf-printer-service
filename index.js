d1bd4e2f7995def92b487ed9b3f6498630051cb4
Mudassir Malik <Malikjrw147@gmail.com>
2022-08-26 15:03:17 +0500

    Winston logger added

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const ptp = require("unix-print");
 const path = require("path");
+const logger = require("./logger");
 const app = express();
 const public = path.join(__dirname, 'public');
-const port = 3000;
+const port = process.env.PORT || 9000;
 const pdfFile = './assets/dummy.pdf';
-
 app.get('/', function (req, res) {
     res.sendFile(path.join(public, 'index.html'));
 });
@@ -17,14 +17,27 @@ app.use('/print', async (req, res) => {
     // const defaultPrinter = await ptp.getDefaultPrinter();
     // console.log('printers', printers);
     // console.log('defaultPrinter', defaultPrinter);
-    const printer = 'Rollo';
-    const options = ["-o landscape", "-o fit-to-page", "-o media=A4"];
-    const result = await ptp.print(pdfFile, printer, options);
-    console.log('result', result);
-    res.status(204);
-    res.send();
+    // const printer = 'Rollo';
+    // const options = ["-o landscape", "-o fit-to-page", "-o media=A4"];
+    // const result = await ptp.print(pdfFile, printer, options);
+    try {
+        const result = await ptp.print(pdfFile);
+        console.log('result', result);
+        logger.info(`Print result -> ${JSON.stringify(result)}`)
+        res.status(204);
+        res.send();
+    } catch (error) {
+        logger.info(`Print error -> ${JSON.stringify(error)}`)
+        res.status(204);
+        res.send();
+    }
+
 })
 
-app.listen(port, () => {
-    console.log(`PDF Printing Service listening on http://localhost:${port}`)
+app.listen(port, async () => {
+    const printers = await ptp.getPrinters();
+    const defaultPrinter = await ptp.getDefaultPrinter();
+    logger.info(`Available Printers -> ${JSON.stringify(printers)}`)
+    logger.info(`defaultPrinter -> ${JSON.stringify(defaultPrinter)}`)
+    console.log(`${process.env.PROJECT} is listening on http://localhost:${port}`)
 });
---
+++ logger.js
@@ -0,0 +1,22 @@
+const winston = require('winston');
+const dotenv = require('dotenv');
+dotenv.config();
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.prettyPrint()
+    ),
+    defaultMeta: { service: process.env.PROJECT },
+    transports: [
+        new winston.transports.File({
+            filename: 'logs/error.log',
+            level: 'error',
+        }),
+        new winston.transports.File({ filename: 'logs/all.log' }),
+        // TODO: uncomment to enable winston logging on the console
+        //new winston.transports.Console(),
+    ],
+});
+
+module.exports = logger;
